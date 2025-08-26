cfd6d27dfa0b2db3f629ad6b51e3889ba39a6df8
charith <tilershub@gmail.com>
2025-08-26 16:12:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import BannerCarousel, { Promo } from '@/components/BannerCarousel';
 import ServiceScroller, { ServiceBanner } from '@/components/ServiceScroller';
 import TilerScroller, { Tiler } from '@/components/TilerScroller';
-
+import BadgeLegend from '@/components/BadgeLegend';
 
 const promoSlides: Promo[] = [
   { id:'p1', image:'/img/promo-1.jpg', title:'Certified Pros', subtitle:'Book a master tiler', ctaLabel:'Get an Estimate', ctaHref:'/estimator' },
@@ -15,17 +15,65 @@ const serviceBanners: ServiceBanner[] = [
 ];
 
 const tilers: Tiler[] = [
-  { id:'t1', name:'Kelum Srimal', location:'Gampaha', rating:4.9, jobsCompleted:157, avatar:'/img/tilers/kelum.jpg', badges:['TILERSHUB Certified'], tags:['Bathroom Tiling','Floor Tiling'], profileHref:'/tilers/kelum-srimal', quoteHref:'/request-quote?tiler=kelum-srimal' },
-  { id:'t2', name:'Saman Anurudda', location:'Kandy', rating:4.8, jobsCompleted:137, avatar:'/img/tilers/saman.jpg', badges:['Master Tiler'], tags:['Bathroom Tiling','Mosaic','Waterproofing'], profileHref:'/tilers/saman-anurudda', quoteHref:'/request-quote?tiler=saman-anurudda' },
-  { id:'t3', name:'Keshara Tile Art', location:'Matara', rating:4.7, jobsCompleted:121, avatar:'/img/tilers/keshara.jpg', badges:['Certified'], tags:['Floor Tiling','Skirting','Stairs/Nosing'] },
-  { id:'t4', name:'Ranarata Tile Art', location:'Anuradhapura', rating:4.8, jobsCompleted:164, avatar:'/img/tilers/ranarata.jpg', badges:['Verified'], tags:['Large Format','Bathroom','Grout/Finish'] },
-  { id:'t5', name:'Kelum Srimal (Team)', location:'Colombo', rating:5.0, jobsCompleted:210, avatar:'/img/tilers/kelum-team.jpg', badges:['Top Rated'], tags:['Bathroom','Floor','Waterproofing'] },
+  {
+    id:'t1',
+    name:'Kelum Srimal',
+    location:'Gampaha',
+    rating:4.9,
+    jobsCompleted:157,
+    avatar:'/img/tilers/kelum.jpg',
+    badges:['Master Tiler','Verified Tiler'], // highest shows under avatar
+    tags:['Bathroom Tiling','Floor Tiling'],
+    profileHref:'/tilers/kelum-srimal',
+    quoteHref:'/request-quote?tiler=kelum-srimal'
+  },
+  {
+    id:'t2',
+    name:'Saman Anurudda',
+    location:'Kandy',
+    rating:4.8,
+    jobsCompleted:137,
+    avatar:'/img/tilers/saman.jpg',
+    badges:['Professional Tiler','Verified Tiler'],
+    tags:['Bathroom Tiling','Mosaic','Waterproofing'],
+    profileHref:'/tilers/saman-anurudda',
+    quoteHref:'/request-quote?tiler=saman-anurudda'
+  },
+  {
+    id:'t3',
+    name:'Keshara Tile Art',
+    location:'Matara',
+    rating:4.7,
+    jobsCompleted:121,
+    avatar:'/img/tilers/keshara.jpg',
+    badges:['Certified Tiler','Verified Tiler'],
+    tags:['Floor Tiling','Skirting','Stairs/Nosing']
+  },
+  {
+    id:'t4',
+    name:'Ranarata Tile Art',
+    location:'Anuradhapura',
+    rating:4.8,
+    jobsCompleted:164,
+    avatar:'/img/tilers/ranarata.jpg',
+    badges:['Professional Tiler','Verified Tiler'],
+    tags:['Large Format','Bathroom','Grout/Finish']
+  },
+  {
+    id:'t5',
+    name:'Kelum Srimal (Team)',
+    location:'Colombo',
+    rating:5.0,
+    jobsCompleted:210,
+    avatar:'/img/tilers/kelum-team.jpg',
+    badges:['Master Tiler','Verified Tiler'],
+    tags:['Bathroom','Floor','Waterproofing']
+  },
 ];
 
 export default function Home() {
   return (
     <main className="bg-slate-50">
-
       <div className="mx-auto max-w-[1100px] px-4 py-4">
         <BannerCarousel slides={promoSlides} />
 
@@ -37,6 +85,7 @@ export default function Home() {
 
         <ServiceScroller services={serviceBanners} />
         <TilerScroller tilers={tilers} />
+        <BadgeLegend />
       </div>
     </main>
   );
