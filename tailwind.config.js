adfd04f3811e9763cc1f25e0c7964e2a7babc03d
charith <tilershub@gmail.com>
2025-08-26 13:21:46 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",    // App Router
+    "./pages/**/*.{js,ts,jsx,tsx}",  // Pages Router (if any)
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
